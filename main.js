1e11e9aaf142b5e1bdbeaccc1f41881e6128df8a
Marek-Jeziorski <111072844+Marek-Jeziorski@users.noreply.github.com>
2023-09-17 17:46:49 +0200

    slideShow cleaning

--- main.js
+++ main.js
@@ -1,62 +1,59 @@
-// GET API-DATA (all-breeds list)
-/* -------------------------------------------------------------------------- */
-
-async function getDogList() {
+async function getAllBreedsList() {
   const response = await fetch('https://dog.ceo/api/breeds/list/all');
   const data = await response.json();
-  createBreedList(data.message);
+  createBreedListMarkup(data.message);
 }
-getDogList();
+getAllBreedsList();
 
-// MARKUP <SELECT> and fill with API-DATA (all-breeds list)
-// <SELECT>-EVENT on CHANGE VALUE  (at choosing one breed)
-/* -------------------------------------------------------------------------- */
-function createBreedList(breedListObject) {
-  const breedList = Object.keys(breedListObject);
+function createBreedListMarkup(apiAllBreedsList) {
+  const allBreedsList = Object.keys(apiAllBreedsList);
   document.getElementById('breed').innerHTML = `
   <select onchange="loadByBreed(this.value)">
     <option>Wybierz rasę psa</option>
-    ${breedList
-      .map(function (breed) {
-        return `<option> ${breed}</option>`;
+    ${allBreedsList
+      .map(function (curentBreed) {
+        return `<option> ${curentBreed}</option>`;
       })
       .join(' ')}
   </select>  
   `;
 }
 
-// GET API-DATA (one breed [image's url-list]))
-/* -------------------------------------------------------------------------- */
-async function loadByBreed(clickedBreed) {
-  if (clickedBreed != 'Wybierz rasę psa') {
+// Function called by <select> onchange event
+async function loadByBreed(selectedBreed) {
+  if (selectedBreed != 'Wybierz rasę psa') {
     const response = await fetch(
-      `https://dog.ceo/api/breed/${clickedBreed}/images`
+      `https://dog.ceo/api/breed/${selectedBreed}/images`
     );
     const data = await response.json();
     createSlideShow(data.message);
   }
 }
 
-// MARKUP <div> x 3 with background-image url's from [IMAGES]
-/* -------------------------------------------------------------------------- */
-function createSlideShow(images) {
-  let slideShow = document.getElementById('slideshow');
+function createSlideShow(breedImages) {
   let currentImage = 0;
-  slideShow.innerHTML = `<div class="slide" style="background-image: url('${images[0]}');"></div>
-  <div class="slide" style="background-image: url('${images[1]}');"></div>`;
+  let slideShow = document.getElementById('slideshow');
+  slideShow.innerHTML = `<div class="slide" style="background-image: url('${breedImages[0]}');"></div>
+  <div class="slide" style="background-image: url('${breedImages[1]}');"></div>`;
+
   currentImage += 2;
   setInterval(nextSlide, 3000);
 
   function nextSlide() {
+    console.log('next slide after 3sec');
     slideShow.insertAdjacentHTML(
       'beforeend',
-      `<div class="slide" style="background-image: url('${images[currentImage]}');"></div>`
+      `<div class="slide" style="background-image: url('${breedImages[currentImage]}');"></div>`
     );
-    setTimeout(() => document.querySelector('.slide').remove(), 1000);
-    if (currentImage + 1 >= images.length) {
+    setTimeout(() => {
+      document.querySelector('.slide').remove();
+      console.log('remove slide after 1sec');
+    }, 1000);
+
+    if (currentImage + 1 >= breedImages.length) {
       currentImage = 0;
     } else {
-      currentPosition++;
+      currentImage++;
     }
   }
 }
